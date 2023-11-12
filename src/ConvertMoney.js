c428cd9e0aaeacda3add338f72dbe055b339e45b
glaubervilane <glauberfmarques@gmail.com>
2023-11-11 21:24:49 -0700

    Improve desig and add alert funcionality

--- src/ConvertMoney.js
+++ src/ConvertMoney.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import './ConvertMoney.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import { Alert } from 'react-bootstrap';
+import { faAngleDoubleRight, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeroImage from './images/hero.jpg';
 
 const ConvertMoney = () => {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState('USD');
   const [toCurrency, setToCurrency] = useState('BRL');
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Implement the currency conversion logic here
-    // ...
+    // Set loading to true to show the spinner
+    setLoading(true);
+
+    // Simulate an asynchronous operation (e.g., API call)
+    setTimeout(() => {
+      // Reset loading state after the operation is complete
+      setLoading(false);
+    }, 2000);
   };
 
   return (
@@ -24,6 +32,9 @@ const ConvertMoney = () => {
           <p>Convert money easily with this tool</p>
         </div>
       </section>
+      <Alert variant="danger" show={false}>
+        Error fetching convertion rate!
+      </Alert>
       <form className="convert-money-form" onSubmit={handleSubmit}>
         <div className="form-row">
           <input
@@ -63,8 +74,9 @@ const ConvertMoney = () => {
             <option>GBP</option>
             <option>ARS</option>
           </select>
-          <button type="submit" className="convert-button">
-            Convert
+          <button type="submit" className="convert-button" disabled={loading}>
+            {loading && <FontAwesomeIcon icon={faSpinner} spin style={{ marginRight: '5px' }} />}
+            {loading ? 'Converting...' : 'Convert'}
           </button>
         </div>
       </form>
