b9750edb18f935dcbb5a6168b1e455aeca673dd8
glaubervilane <glauberfmarques@gmail.com>
2023-11-13 09:08:52 -0700

    Finish Fixer API interaction and make select pull data in real time

--- src/ConvertMoney.js
+++ src/ConvertMoney.js
@@ -1,29 +1,84 @@
 // ConvertMoney.js
 
 import React, { useState } from 'react';
-import './ConvertMoney.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Alert, Modal } from 'react-bootstrap';
 import { faAngleDoubleRight, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import HeroImage from './images/hero.jpg';
+import axios from 'axios';
 import ListCurrency from './components/ListCurrency';
+import HeroImage from './images/hero.jpg';
+import './ConvertMoney.css';
+
+const FIXER_API_KEY = process.env.REACT_APP_FIXER_API_KEY;
+const FIXER_URL = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
+
 
 const ConvertMoney = () => {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState('USD');
-  const [toCurrency, setToCurrency] = useState('BRL');
+  const [toCurrency, setToCurrency] = useState('CAD');
   const [loading, setLoading] = useState(false);
+  const [formValidated, setFormValidated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [conversionResult, setConversionResult] = useState(null);
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Set loading to true to show the spinner
     setLoading(true);
-
-    // Simulate an asynchronous operation (e.g., API call)
     setTimeout(() => {
-      // Reset loading state after the operation is complete
       setLoading(false);
     }, 2000);
+    setFormValidated(true);
+    convert();
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setAmount(1);
+    setFromCurrency('USD');
+    setToCurrency('CAD');
+    setFormValidated(false);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value.replace(/\D/, ''));
+  };
+
+  const convert = () => {
+    setFormValidated(true);
+    setLoading(true);
+
+    axios.get(FIXER_URL)
+      .then(res => {
+        const cotacao = getQuotation(res.data);
+        if (cotacao) {
+          setConversionResult(`${amount} ${fromCurrency} = ${cotacao} ${toCurrency}`);
+          setShowModal(true);
+          setLoading(false);
+          setError(false);
+        } else {
+          showError();
+        }
+      })
+      .catch(() => {
+        showError();
+      });
+  };
+
+  const getQuotation = (quoteData) => {
+    if (!quoteData || quoteData.success !== true) {
+      return false;
+    }
+    const quotationOf = quoteData.rates[fromCurrency];
+    const quotationFor = quoteData.rates[toCurrency];
+    const quotation = (1 / quotationOf * quotationFor) * amount;
+    return quotation.toFixed(2);
+  };
+
+  const showError = () => {
+    setError(true);
+    setLoading(false);
   };
 
   return (
@@ -35,21 +90,21 @@ const ConvertMoney = () => {
           <p>Convert money easily with this tool</p>
         </div>
       </section>
-      <Alert variant="danger" show={false}>
+      <Alert variant="danger" show={error}>
         Error fetching conversion rate!
       </Alert>
-      <form className="convert-money-form" onSubmit={handleSubmit}>
+      <form className={`convert-money-form ${formValidated ? 'was-validated' : ''}`} onSubmit={handleSubmit}>
         <div className="form-row">
           <input
             type="number"
-            className="amount-input"
+            className={`amount-input ${formValidated && !amount ? 'is-invalid' : ''}`}
             placeholder="0"
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={handleAmountChange}
             required
           />
           <select
-            className="currency-select"
+            className={`currency-select ${formValidated && !fromCurrency ? 'is-invalid' : ''}`}
             value={fromCurrency}
             onChange={(e) => setFromCurrency(e.target.value)}
             required
@@ -60,7 +115,7 @@ const ConvertMoney = () => {
             <FontAwesomeIcon icon={faAngleDoubleRight} />
           </div>
           <select
-            className="currency-select"
+            className={`currency-select ${formValidated && !toCurrency ? 'is-invalid' : ''}`}
             value={toCurrency}
             onChange={(e) => setToCurrency(e.target.value)}
             required
@@ -73,15 +128,15 @@ const ConvertMoney = () => {
           </button>
         </div>
       </form>
-      <Modal show={false}>
+      <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>Conversion Result</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          1 USD = 5.22 BRL
+          {conversionResult}
         </Modal.Body>
         <Modal.Footer>
-          <button variant="success" type="button" className="btn btn-primary">
+          <button variant="success" type="button" className="btn btn-primary" onClick={handleCloseModal}>
             New Conversion
           </button>
         </Modal.Footer>
